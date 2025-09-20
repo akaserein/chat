16457132e83078ac39182580e0fb629ebe032f18
akaserein <himojoit1234@gmail.com>
2025-09-20 14:51:01 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAkrJywdCAhPm8VbV3Ji0Ktkdc4m2UIj1Y",
+  authDomain: "chatmark-u.firebaseapp.com",
+  databaseURL: "https://chatmark-u-default-rtdb.firebaseio.com/",
+  projectId: "chatmark-u",
+  storageBucket: "chatmark-u.appspot.com",
+  messagingSenderId: "682637917512",
+  appId: "1:682637917512:web:ea41aaa8e9af4bdf490770",
+  measurementId: "G-H941JPNK3D"
+};
